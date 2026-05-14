f9825021bd4ace54fdcd66871e7621b89f5de16e
Daniel Orellana <daniel.orellana.ic@gmail.com>
2026-05-14 02:24:54 -0400

    force update UI

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,32 +1,112 @@
+import "./ProgressBar.css";
+
 export default function ProgressBar({
   caught,
   total,
 }) {
+
   const percent =
     total === 0
       ? 0
-      : (caught / total) * 100;
+      : Math.floor((caught / total) * 100);
+
+  const missing = total - caught;
 
   return (
-    <div className="progress-container">
 
-      <div className="progress-text">
-        <span>Progress</span>
+    <div className="progress-card">
 
-        <span>
-          {caught}/{total}
-        </span>
+      {/* titulo */}
+      <div className="progress-header">
+
+        <div>
+
+          <h2>
+            Master Pokédex
+          </h2>
+
+          <p>
+            Track your Pokémon journey
+          </p>
+
+        </div>
+
       </div>
 
-      <div className="progress-bar">
-        <div
-          className="progress-fill"
+      {/* contenido */}
+      <div className="progress-content">
+
+        {/* circulo */}
+        <div 
+          className="progress-circle"
           style={{
-            width: `${percent}%`,
+            "--percent": percent,
           }}
-        />
+        > 
+
+          <div className="circle-inner">
+
+            <h3>
+              {caught}
+            </h3>
+
+            <span>
+              / {total}
+            </span>
+
+            <p>
+              {percent}%
+            </p>
+
+          </div>
+
+        </div>
+
+        {/* stats */}
+        <div className="progress-stats">
+
+          <div className="stat caught">
+
+            <span>
+              {caught}
+            </span>
+
+            <p>
+              Caught
+            </p>
+
+          </div>
+
+          <div className="stat missing">
+
+            <span>
+              {missing}
+            </span>
+
+            <p>
+              Missing
+            </p>
+
+          </div>
+
+          <div className="stat total">
+
+            <span>
+              {total}
+            </span>
+
+            <p>
+              Total
+            </p>
+
+          </div>
+
+        </div>
+
       </div>
 
     </div>
+
   );
+
 }
