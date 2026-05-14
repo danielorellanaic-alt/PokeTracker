cc4635f85d8b32e50b619b6379d1a1c73909caa9
Daniel Orellana <daniel.orellana.ic@gmail.com>
2026-05-13 23:01:41 -0400

    force update UI

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,3 +1,5 @@
+import "./PokemonCard.css";
+
 export default function PokemonCard({
   pokemon,
   caught,
@@ -5,92 +7,46 @@ export default function PokemonCard({
 }) {
 
   return (
+
     <div
+      className="pokemon-card"
       onClick={() => toggle(pokemon.id)}
-      className="
-        relative
-        overflow-hidden
-        w-full
-        h-28
-        rounded-3xl
-        px-5
-        flex
-        items-center
-        justify-between
-        shadow-md
-        transition-all
-        active:scale-[0.98]
-        bg-gradient-to-r
-        from-green-200
-        to-purple-200
-      "
     >
 
-      {/* numero grande fondo */}
-      <span
-        className="
-          absolute
-          right-4
-          bottom-0
-          text-6xl
-          font-black
-          text-white/40
-          select-none
-        "
-      >
-        #
-        {String(pokemon.id).padStart(3, "0")}
-      </span>
+      {/* número */}
+      <div className="pokemon-number">
+        #{String(pokemon.id).padStart(3, "0")}
+      </div>
 
-      {/* lado izquierdo */}
-      <div className="flex items-center gap-4 z-10">
+      {/* izquierda */}
+      <div className="pokemon-left">
 
-        {/* imagen */}
         <img
           src={pokemon.image}
           alt={pokemon.name}
-          className="
-            w-24
-            h-24
-            object-contain
-            drop-shadow-md
-          "
+          className="pokemon-image"
         />
 
-        {/* nombre */}
-        <div className="flex flex-col">
+        <div className="pokemon-info">
 
-          <span
-            className="
-              text-3xl
-              font-bold
-              text-white
-              capitalize
-            "
-          >
+          <h2>
             {pokemon.name}
-          </span>
+          </h2>
 
-          {/* estado capturado */}
-          <div className="mt-2">
-            <div
-              className={`
-                w-4
-                h-4
-                rounded-full
-                ${
-                  caught
-                    ? "bg-green-500"
-                    : "bg-white/50"
-                }
-              `}
-            />
-          </div>
+          <div
+            className={
+              caught
+                ? "caught active"
+                : "caught"
+            }
+          />
 
         </div>
 
       </div>
 
     </div>
+
   );
+
 }
