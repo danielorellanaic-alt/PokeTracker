259edbd238784756d83ffc0d2cc12938c5dc0b19
Daniel Orellana <daniel.orellana.ic@gmail.com>
2026-05-16 01:30:09 -0400

    Updated UI

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -3,8 +3,7 @@ import { spriteOverrides } from "../data/spriteOverrides";
 import { typeIcons } from "../data/typeIcons";
 import { typeTranslations } from "../data/typeTranslations";
 import Icon from "@mdi/react";
-import { mdiPokeball } from "@mdi/js";
-
+import { mdiPokeballOutline } from "@mdi/js";
 
 function hasSpecialForm(pokemon) {
   const id = pokemon.id;
@@ -92,7 +91,7 @@ export default function PokemonCard({
 
       {caught && (
         <div className="caught-icon-badge">
-          <Icon path={mdiPokeball} size={0.9} />
+          <Icon path={mdiPokeballOutline} size={0.9} />
         </div>
       )}
     </div>
