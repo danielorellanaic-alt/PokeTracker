ff533d4bc0e43624193c5779a3c7e597630b61a4
Daniel Orellana <daniel.orellana.ic@gmail.com>
2026-05-17 00:02:50 -0400

    Updated Pokédex

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -10,6 +10,7 @@ function hasSpecialForm(pokemon) {
   return (
     id.endsWith("M1") ||
     id.endsWith("M2") ||
+    id.endsWith("M3") ||
     id.endsWith("GG") ||
     id.endsWith("PB") ||
     id.endsWith("SB") ||
@@ -33,90 +34,50 @@ function hasSpecialForm(pokemon) {
     id.endsWith("A") ||
     id.endsWith("G") ||
     id.endsWith("H") ||
-
+    
+    name.includes("disguised") ||
     name.includes("incarnate") ||
+
     name.includes("galarian") ||
-    name.includes("therian") ||
     name.includes("gigamax") ||
     name.includes("hisuian") ||
     name.includes("paldean") ||
     name.includes("defense") ||
     name.includes("altered") ||
+
+    name.includes("therian") ||
+    name.includes("eternal") ||
+    name.includes("pom-pom") ||
+
     name.includes("alolan") ||
-    name.includes("attack") ||
     name.includes("origin") ||
     name.includes("primal") ||
-    name.includes("speed") ||
-    name.includes("sunny") ||
     name.includes("rainy") ||
     name.includes("snowy") ||
-    name.includes("mega") ||
-    name.includes("land") ||
-    name.includes("sky") ||
-    name.includes("east") ||
-    name.includes("west") ||
-    name.includes("overgrown") ||
-    name.includes("ice") ||
-    name.includes("shadow") ||
-    name.includes("dusk") ||
-    name.includes("dawn") ||
-    name.includes("school") ||
-    name.includes("solo") ||
-    name.includes("baile") ||
-    name.includes("pom-pom") ||
-    name.includes("pa'u") ||
-    name.includes("sensu") ||
+    name.includes("attack") ||
     name.includes("crowned") ||
-    name.includes("eternal") ||
-    name.includes("dada") ||
-    name.includes("disguised") ||
-    name.includes("gmax") ||
-    name.includes("alola") ||
+
+    name.includes("paldea") ||
     name.includes("galar") ||
     name.includes("hisui") ||
-    name.includes("aqua breed") ||
-    name.includes("blaze breed") ||
-    name.includes("combat breed") ||
-    name.includes("paldea") ||
-    name.includes("tauros") ||
-    name.includes("castform") ||
-    name.includes("deoxys") ||
-    name.includes("mimikyu") ||
-    name.includes("lycanroc") ||
-    name.includes("wishiwashi") ||
-    name.includes("minior") ||
-    name.includes("necrozma") ||
-    name.includes("oricorio") ||
-    name.includes("eiscue") ||
-    name.includes("indeedee") ||
-    name.includes("morpeko") ||
-    name.includes("urshifu") ||
-    name.includes("zarude") ||
-    name.includes("calyrex") ||
-    name.includes("enamorus") ||
-    name.includes("flabébé") ||
-    name.includes("floette") ||
-    name.includes("florges") ||
-    name.includes("meowstic") ||
-    name.includes("aegislash") ||
-    name.includes("pumpkaboo") ||
-    name.includes("gourgeist") ||
-    name.includes("xurkitree") ||
-    name.includes("celesteela") ||
-    name.includes("kartana") ||
-    name.includes("guzzlord") ||
-    name.includes("necrozma") ||
-    name.includes("magearna") ||
-    name.includes("marshadow") ||
-    name.includes("zeraora") ||
-    name.includes("meltan") ||
-    name.includes("melmetal") ||
-    name.includes("grookey") ||
-    name.includes("scorbunny") ||
-    name.includes("sobble") ||
-    name.includes("rillaboom") ||
-    name.includes("cinderace") ||
-    name.includes("raichu")
+    name.includes("sunny") ||
+    name.includes("school") ||
+    name.includes("baile") ||
+    name.includes("sensu") ||
+
+    name.includes("mega") ||
+    name.includes("gmax") ||
+    name.includes("speed") ||
+    name.includes("dawn") ||
+    name.includes("dusk") ||
+    name.includes("solo") ||
+    name.includes("land") ||
+    name.includes("west") ||
+    name.includes("east") ||
+    name.includes("sky") ||
+
+    name.includes("alola") ||
+    name.includes("pa'u")
   );
 }
 
