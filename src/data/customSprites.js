971e2654801db9033969590c15236f75c029ab9c
Daniel Orellana <daniel.orellana.ic@gmail.com>
2026-05-17 00:59:51 -0400

    Updated Pokédex

--- src/data/customSprites.js
+++ src/data/customSprites.js
@@ -1,6 +1,8 @@
 import wormadamPlant from "../assets/custom-sprites/0413PW.png";
 import wormadamSandy from "../assets/custom-sprites/0413SW.png";
 import wormadamTrash from "../assets/custom-sprites/0413TW.png";
+import megaraichux from "../assets/custom-sprites/0026M1.png";
+import megaraichuy from "../assets/custom-sprites/0026M2.png";
 
 export const customSprites = {
   "0413PW": wormadamPlant,
