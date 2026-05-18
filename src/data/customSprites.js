91b3ac7301887bfca0e917527614606702a9571a
Daniel Orellana <daniel.orellana.ic@gmail.com>
2026-05-17 23:07:16 -0400

    Updated Pokédex

--- src/data/customSprites.js
+++ src/data/customSprites.js
@@ -4,6 +4,8 @@ import wormadamTrash from "../assets/custom-sprites/0413TW.png";
 import megaraichux from "../assets/custom-sprites/0026M1.png";
 import megaraichuy from "../assets/custom-sprites/0026M2.png";
 import burmyTrash from "../assets/custom-sprites/0412TB.png";
+import burmyPlant from "../assets/custom-sprites/0412PB.png";
+import burmySandy from "../assets/custom-sprites/0412SB.png";
 
 export const customSprites = {
   "0413PW": wormadamPlant,
@@ -12,4 +14,7 @@ export const customSprites = {
   "0026M1": megaraichux,
   "0026M2": megaraichuy,
   "0412TB": burmyTrash,
+  "0412PB": burmyPlant,
+  "0412SB": burmySandy,
+
 };
